7063a9c60f8f32e690d2ca3fd5af11946502a778
Pasan Jayathilaka <pasan4all@gmail.com>
2020-01-23 12:31:11 +0530

    22-01-2020 1231hrs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Items in your cart:{cartItems.toString()}</p>
+        <button
+          onClick={() => {
+            setCartItems([...cartItems, "apple"]);
+          }}
+        />
       </header>
     </div>
   );
